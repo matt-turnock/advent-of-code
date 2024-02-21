4799854f31bb2d3d67c964d7f473fb6fae16a456
Matt Turnock <matt.p.turnock@gmail.com>
2024-02-21 11:51:49 +0000

    Adding day5, and started bits in 2023

--- 2015/day5/naughty_or_nice.py
+++ 2015/day5/naughty_or_nice.py
@@ -1,58 +1,30 @@
-def bad_string(line):
-    if 'ab' in line or 'cd' in line or 'pq' in line or 'xy' in line:
-        return False
-    else:
-        return True
+import re
 
 
-def doubleletter(line):
-    for index in range(len(line) - 1):
-        if line[index] == line[index + 1]:
-            return True
+def part1():
+    count = 0
 
-    return False
-
-
-def vowel_check(line):
-    vowels = ['a', 'e', 'i', 'o', 'u']
-    total_vowels = []
-
-    for letter in line:
-        if letter in vowels:
-            total_vowels.append(letter)
-
-    if len(total_vowels) > 2:
-        return True
-    else:
-        return False
-
-
-def pair(line):
-    for index in range(len(line) - 1):
-        if f"{line[index]}{line[index + 1]}" in line[index + 2:]:
-            return True
-        else:
-            return False
-
-
-def sandwich(line):
-    for index in range(len(line) - 1):
-        
-
-
-nice_stringsv1 = []
-nice_stringsv2 = []
-
-try:
-    with open('./strings', 'r') as f:
+    with open('strings', 'r') as f:
         for line in f:
-            if bad_string(line) and doubleletter(line) and vowel_check(line):
-                nice_stringsv1.append(line)
-            if pair(line) and sandwich(line):
-                nice_stringsv2.append(line)
-except FileNotFoundError as error:
-    print(error)
+            if re.search(r'(.*[aeiou]){3}', line):
+                if re.search(r'(.)\1', line):
+                    if not re.search(r'ab|cd|pq|xy', line):
+                        count += 1
+
+    return count
 
 
-print(f"Part1: {len(nice_stringsv1)}")
-print(f"Part2: {len(nice_stringsv2)}")
+def part2():
+    count = 0
+
+    with open('strings', 'r') as f:
+        for line in f:
+            if re.search(r'(..).*\1', line):
+                if re.search(r'(.).\1', line):
+                    count += 1
+
+    return count
+
+
+print(part1())
+print(part2())
